eb85d2d3a10591c5e488ac6a3e3bf6e3fe502742
Eric-Gona <eric.gona254@gmail.com>
2022-03-20 20:27:19 +0300

    creates models in models.py

--- apphood/models.py
+++ apphood/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.db.models.signals import post_save
+from PIL import Image
+
